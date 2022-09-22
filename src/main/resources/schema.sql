f9024f6314c713745f557e1a853e9d61517f724f
jekanest <jekanest@yahoo.com>
2022-09-22 12:34:12 +0300

    Test for Jenkins

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS shelters_pets;
 CREATE TABLE shelters_pets (
    pet_id BIGINT AUTO_INCREMENT NOT NULL,
    pet_name VARCHAR(255),
-   pet_date_of_birth VARCHAR(255),
+   pet_date_of_birth VARCHAR(255)
    pet_age_in_months BIGINT,
    pet_registration_date VARCHAR(255),
    pet_type VARCHAR(255),
